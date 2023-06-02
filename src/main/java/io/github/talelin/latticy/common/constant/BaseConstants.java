5474958b8f145c8ced0b4cd31a93b52380e7e8a6
nj <1021204224@qq.com>
2023-06-02 14:08:46 +0800

    :fire:

--- src/main/java/io/github/talelin/latticy/common/constant/BaseConstants.java
+++ src/main/java/io/github/talelin/latticy/common/constant/BaseConstants.java
@@ -1,19 +1,7 @@
 package io.github.talelin.latticy.common.constant;
 
 public interface BaseConstants {
-    String VERSION = "1.8.3.RELEASE";
     Integer BASIC_SUCCESS_CODE = 8000000;
     Integer BASIC_ERROR_CODE = -8000000;
-    String LOCAL_IP = "127.0.0.1";
-    String DEFAULT_MAIL_FROM = "mailadmin@ecidi.com";
-    String TEST = "test";
-    String TEST_NAME = "测试用户";
-    String SYSTEM = "system";
-    String SYSTEM_NAME = "系统用户";
-    Integer DEFAULT_TENANT_ID = 100000;
-    String FAWKES_SESSION_NAME = "fawkes-session-n";
-    String DEV_CODE = "dev";
-    String PROD_CODE = "prod";
-    String TEST_CODE = "test";
 }
 
